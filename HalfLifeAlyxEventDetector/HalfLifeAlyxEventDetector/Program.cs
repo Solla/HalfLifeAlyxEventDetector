37707e6b302d5ec4371ff9b5d6a991180356df8f
Solla <willieyuwei4@gmail.com>
2021-05-04 03:55:47 +0800

    First Implement

---
+++ HalfLifeAlyxEventDetector/HalfLifeAlyxEventDetector/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Threading;
+using static HalfLifeAlyxEventDetector.HalfLifeAlyxGameMonitor;
+
+namespace HalfLifeAlyxEventDetector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!Environment.Is64BitProcess)
+                throw new Exception("Please build this application with 64 bit");
+            HalfLifeAlyx_Manager HLA_Manager = new HalfLifeAlyx_Manager();
+            HalfLifeAlyx_Autoexec HLA_Autoexec =
+                new HalfLifeAlyx_Autoexec().
+                UnlimitedMagazineInBag(true).
+                GiveAllUnlockedWeapons(true);
+            if (!HLA_Manager.RestartHalfLifeAlyx(HLA_Autoexec))
+                throw new Exception("Cannot open Half-Life: Alyx!");
+            HalfLifeAlyxGameMonitor GameMonitor = new HalfLifeAlyxGameMonitor();
+            GameMonitor.RegisterEventHandler(EventCallbackHandler);
+            GameMonitor
+                .SetBuddha(true)
+                .Map("a1_intro_world");
+            Console.ReadLine();
+        }
+        static void EventCallbackHandler(NowWeaponType WeaponType, HapticEventType Haptic)
+        {
+            if (Haptic == HapticEventType.WeaponShoot)
+                ShootHapticFeedback(WeaponType);
+            else if (Haptic == HapticEventType.WeaponChange)
+                SwitchWeaponHapticFeedback(WeaponType);
+            else if (Haptic == HapticEventType.InsertBullet)
+                InsertBulletHapticFeedback(WeaponType);
+            else if (Haptic == HapticEventType.LeftHandGrabStart || Haptic == HapticEventType.RightHandGrabStart)
+                GloveStartGrab(Haptic == HapticEventType.RightHandGrabStart);
+            else if (Haptic == HapticEventType.RightHandGrabStop || Haptic == HapticEventType.RightHandPickupItem)
+                GloveStopGrab(true);
+            else if (Haptic == HapticEventType.LeftHandGrabStop || Haptic == HapticEventType.LeftHandPickupItem)
+                GloveStopGrab(false);
+            else if (Haptic == HapticEventType.PlayerTeleportStart)
+                StartTeleportation();
+            else if (WeaponType == NowWeaponType.Energygun && Haptic == HapticEventType.EnergygunEjectClip)
+                EnergygunEjectClip();
+            else if (WeaponType == NowWeaponType.Energygun && Haptic == HapticEventType.ChamberedRound)
+                EnergygunChamberedRound();
+            else if (WeaponType == NowWeaponType.Shotgun && Haptic == HapticEventType.Shotgun_Open)
+                Shotgun_Open();
+            else if (WeaponType == NowWeaponType.Shotgun && Haptic == HapticEventType.Shotgun_Close)
+                Shotgun_Close();
+            else if (WeaponType == NowWeaponType.Shotgun && Haptic == HapticEventType.Shotgun_Chambered)
+                Shotgun_Chambered();
+            Console.WriteLine($"Event {Enum.GetName(typeof(HapticEventType), Haptic)} with Weapon {Enum.GetName(typeof(NowWeaponType), WeaponType)}");
+        }
+        private static void ShootHapticFeedback(NowWeaponType WeaponType)
+        {
+            switch (WeaponType)
+            {
+                case NowWeaponType.Energygun:
+                    break;
+                case NowWeaponType.Rapidfire:
+                    break;
+                case NowWeaponType.Shotgun:
+                    break;
+            }
+        }
+        private static void SwitchWeaponHapticFeedback(NowWeaponType WeaponType)
+        {
+            switch (WeaponType)
+            {
+                case NowWeaponType.Shotgun:
+                    break;
+                case NowWeaponType.Rapidfire:
+                    break;
+                case NowWeaponType.Energygun:
+                    break;
+            }
+            return;
+        }
+        private static void InsertBulletHapticFeedback(NowWeaponType WeaponType)
+        {
+            switch (WeaponType)
+            {
+                case NowWeaponType.Energygun:
+                    break;
+                case NowWeaponType.Shotgun:
+                    break;
+                case NowWeaponType.Rapidfire:
+                    break;
+            }
+        }
+        static void GloveStartGrab(bool RightHand)
+        {
+        }
+        static void GloveStopGrab(bool RightHand)
+        {
+        }
+        static void StartTeleportation()
+        {
+        }
+
+        static void EnergygunEjectClip()
+        {
+        }
+
+        static void EnergygunChamberedRound()
+        {
+        }
+
+        static void Shotgun_Open()
+        {
+        }
+
+        static void Shotgun_Close()
+        {
+        }
+
+        static void Shotgun_Chambered()
+        {
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,60 @@
+# README
+
+This program is capable of detecting game events in Half-Life: Alyx, such as shooting, teleportating, and grabing stuffs.
+
+Haptic developers can use this program to integrate your haptic devices into Half-Life: Alyx.
+
+The method has been utilized by JetController(CHI'21 Paper, CHI'21 Interactivity, SIGGRAPH'21 Demo).
+
+
+# Caution
+
+## The program is licensed under the GNU General Public License v2.0
+
+When distributing derived works, the source code of the repository must be made available.
+
+It means you should always make your project open-sourced if you want to clone and modify the repository.
+
+### BibTeX for JetController CHI'21 Full Paper
+```
+(Publish after 2021/5/8)
+```
+
+### BibTeX for JetController SIGGRAPH'21 Demo
+```
+(Not Published Yet)
+```
+
+### BibTeX for the repository
+```
+@MISC {HalfLifeAlyxEventDetector,
+    title   = "Half-Life: Alyx Event Detector",
+    url     = "https://github.com/Solla/HalfLifeAlyxEventDetector",
+    year =  "2021",
+    author = "Solla"
+}
+```
+## Citation for Acadamaic Usage
+
+Due to the disclose source policy in GPL v2.0 License, you should include a citation in your paper.
+ 
+As the program is a part of JetController's system, please consider citing JetController for disclosing source if suitable.
+
+## Citation for Other Purpose (Patent / Self-media / Youtuber / Business Usage)
+
+Be aware that you should follow the GPL v2.0 License, so you have to disclose source.
+
+Please cite the repository to disclose source.
+
+Also, it is welcome to cite and introduce our acadamic papers, JetController, if possible.
+
+# Links to JetController
+
+CHI'21 Full Paper
+https://doi.org/10.1145/3411764.3445549
+
+CHI'21 Interactivity (Does not utilize the program) 
+https://doi.org/10.1145/3411763.3451542
+
+SIGGRAPH'21 Demo
+(Not Published Yet)
